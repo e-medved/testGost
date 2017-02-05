511922e78dc48872507def37942eb35d1a3eca31
Medvedev Evgeney <petia256@yandex.ru>
2017-02-06 00:33:18 +0300

    hibernate config review

--- src/main/java/com/gostgroup/test/config/DatabaseConfig.java
+++ src/main/java/com/gostgroup/test/config/DatabaseConfig.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * @author root
@@ -43,19 +39,6 @@ public class DatabaseConfig {
     // Vendor adapter
     HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
     entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-
-    // Hibernate properties
-    Properties additionalProperties = new Properties();
-    additionalProperties.put(
-        "hibernate.dialect",
-        env.getProperty("hibernate.dialect"));
-    additionalProperties.put(
-        "hibernate.show_sql",
-        env.getProperty("hibernate.show_sql"));
-    additionalProperties.put(
-        "hibernate.hbm2ddl.auto",
-        env.getProperty("hibernate.hbm2ddl.auto"));
-    entityManagerFactory.setJpaProperties(additionalProperties);
 
     return entityManagerFactory;
   }
